8f758751d141292d7409c76fe66faa54e8a5d0c6
МИЛАША <k.g_00@mail.ru>
2023-09-22 12:06:20 +0300

    added settings

--- hqprojects/settings.py
+++ hqprojects/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apiapp.apps.ApiappConfig',
+    'hqapp.apps.HqappConfig',
 ]
 
 MIDDLEWARE = [
