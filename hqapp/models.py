3c63f4319202542011e8fc78ac9eafde73a29232
МИЛАША <k.g_00@mail.ru>
2023-09-22 16:17:45 +0300

    added models

--- hqapp/models.py
+++ hqapp/models.py
@@ -10,6 +10,27 @@ class Author(models.Model):
 
     name = models.CharField(max_length=100, verbose_name="name")
 
+    def __str__(self) -> str:
+        return f" {self.name}"
+
+
+class Lesson(models.Model):
+    """Модель урока"""
+
+    # STATUS_VIDEO = [
+    #     ("True", "true"),
+    #     ("False ", "false"),
+    # ]
+
+    title = models.CharField(max_length=200, verbose_name="title")
+    link = models.URLField(max_length=360, verbose_name="link")
+    video_time = models.PositiveIntegerField()
+    # viewed = models.CharField(choices=STATUS_VIDEO, max_length=30, default="false")
+    # product = models.ManyToManyField(Product, related_name="lessons")
+
+    def __str__(self) -> str:
+        return f" {self.title}"
+
 
 class Product(models.Model):
     """Модель товара"""
@@ -20,21 +41,22 @@ class Product(models.Model):
 
     author =  models.ForeignKey(
         Author, on_delete=models.CASCADE, verbose_name="author")
-    lesson = models.ForeignKey(
-        "Lesson", on_delete=models.CASCADE, related_name="lesson")
+    product_name = models.CharField(max_length=300, verbose_name="product_name")
+    lesson = models.ManyToManyField(Lesson, related_name="product")
+
+    def __str__(self) -> str:
+        return f"{self.product_name}"
 
 
-class Lesson(models.Model):
-    """Модель урока"""
+class Student(models.Model):
+    """Модель ученика"""
 
     STATUS_VIDEO = [
         ("True", "true"),
         ("False ", "false"),
     ]
 
-    title = models.CharField(max_length=200, verbose_name="title")
-    link = models.URLField(max_length=360, verbose_name="link")
-    video_time = models.PositiveIntegerField()
-    viewed = models.CharField(choices=STATUS_VIDEO, default="false")
+    name = models.CharField(max_length=250, verbose_name="name")
 
-
+    def __str__(self)-> str:
+        return f"{self.name}"
