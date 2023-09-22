de2a77ea599d5f11367b06ea8799e388be0759bd
МИЛАША <k.g_00@mail.ru>
2023-09-22 11:55:40 +0300

    added models

--- hqapp/models.py
+++ hqapp/models.py
@@ -1,31 +1,40 @@
 from django.db import models
 
 
-class Profile(models.Model):
-    pass
+class Author(models.Model):
+    """Модель владельца видеоурока"""
+
+    class Meta:
+        verbose_name = "author"
+        verbose_name_plural = "authors"
+
+    name = models.CharField(max_length=100, verbose_name="name")
 
 
 class Product(models.Model):
     """Модель товара"""
 
-    # AVAILABLE_CHOICES = [
-    #     ("true", _("true")),
-    #     ("false ", _("false")),
-    # ]
-
     class Meta:
         verbose_name_plural = ("products")
         verbose_name = ("product")
 
-    user =  models.ForeignKey(
-        Profile, on_delete=models.CASCADE, verbose_name="user")
+    author =  models.ForeignKey(
+        Author, on_delete=models.CASCADE, verbose_name="author")
+    lesson = models.ForeignKey(
+        "Lesson", on_delete=models.CASCADE, related_name="lesson")
 
 
 class Lesson(models.Model):
-        """Модель урока"""
+    """Модель урока"""
 
-        class Meta:
-            verbose_name_plural = "lessons"
-            verbose_name = "lesson"
+    STATUS_VIDEO = [
+        ("True", "true"),
+        ("False ", "false"),
+    ]
 
-    title = models.CharField(max_length=200, verbose_name='lesson')
+    title = models.CharField(max_length=200, verbose_name="title")
+    link = models.URLField(max_length=360, verbose_name="link")
+    video_time = models.PositiveIntegerField()
+    viewed = models.CharField(choices=STATUS_VIDEO, default="false")
+
+
