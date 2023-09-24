1ba1a30b484edc6ca2f5280952b73877997bb112
МИЛАША <k.g_00@mail.ru>
2023-09-24 20:27:13 +0300

    changed views

--- apiapp/views.py
+++ apiapp/views.py
@@ -15,16 +15,26 @@ from rest_framework.views import APIView
 from hqapp.models import Product, Student, Lesson
 from .serializers import ProductSerializer, StudentSerializer, LessonSerializer
 
+
 class AllProductsApiView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
-class StudentApiView(ListAPIView):
+class StudentListApiView(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    lookup_field = "pk",
 
+
+class StudentApiView(RetrieveAPIView):
+    """Представление для получения студента."""
+    # queryset = Student.objects.all()
+    # serializer_class = StudentSerializer
+    # lookup_field = "pk"
+    def get(self, request, pk):
+        queryset = Student.objects.get(pk=pk)
+        serializer = StudentSerializer(queryset)
+        return Response(serializer.data)
 
 class LessonApiView(APIView):
 
