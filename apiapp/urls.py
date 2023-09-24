4338385f26e6fc2528fe3055ea6bdee473e5b8a1
МИЛАША <k.g_00@mail.ru>
2023-09-24 20:27:05 +0300

    changed urls

--- apiapp/urls.py
+++ apiapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import AllProductsApiView, StudentApiView, LessonApiView
+from .views import AllProductsApiView, StudentListApiView, LessonApiView, StudentApiView
 
 
 app_name = "apiapp"
 
 urlpatterns = [
     path("products/", AllProductsApiView.as_view(), name='products_list'),
-    path("students/", StudentApiView.as_view(), name="students"),
+    path("students/", StudentListApiView.as_view(), name="students"),
+    path("student/<int:pk>", StudentApiView.as_view(), name="student"),
     path("lessons/", LessonApiView.as_view(), name="lessons")
 
 ]
